4ae9271d35f54acf083e48a0810d5b5dc7fab3ea
root <root@LAPTOP-9AO97945.localdomain>
2019-12-26 18:34:26 +0100

    [UPD] Fichier modifié

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,50 @@
+class PagesController < ApplicationController
+  def programs
+  end
+
+  def hebdo
+  end
+
+  def deco
+  end
+
+  def bootcamp
+  end
+
+  def campus
+  end
+
+  def cergy
+  end
+
+  def pontoise
+  end
+
+  def about_us
+  end
+
+  def contact
+  end
+
+  def postuler
+  end
+
+  def cdv
+  end
+
+  def faq
+  end
+
+  def events
+  end
+
+  def job
+  end
+
+  def legal
+  end
+
+  def merci
+  end
+  
+end
---
+++ config/routes.rb
@@ -0,0 +1,32 @@
+Rails.application.routes.draw do
+
+  
+  
+  
+ 
+
+  get 'pages/job'
+  root 'home#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # routes for static_pages :
+
+get "/contact", to: "pages#contact", :as => 'contact'
+get "/about", to: "pages#about_us", :as => 'about'
+get "/nos_programmes", to: "pages#programs", :as => 'programs'
+get "/nos_ateliers_hebdomadaires", to: "pages#hebdo", :as => 'hebdo'
+get "/nos_ateliers_decouverte", to: "pages#deco", :as => 'deco'
+get "/nos_bootcamps", to: "pages#bootcamp", :as => 'bootcamp'
+get "/nos_campus", to: "pages#campus", :as => 'campus'
+get "/cergy", to: "pages#cergy", :as => 'cergy'
+get "/pontoise", to: "pages#pontoise", :as => 'pontoise'
+get "/postuler", to: "pages#postuler", :as => 'postuler'
+get "/cooodit-gcv", to: "pages#cdv", :as => 'cdv'
+get "/faq", to: "pages#faq", :as => 'faq'
+get "/events", to: "pages#events", :as => 'events'
+get "/nous-rejoindre", to: "pages#job", :as => 'job'
+get "/mentions-legales", to: "pages#legal", :as => 'legal'
+get "/merci", to: "pages#merci", :as => 'merci'
+
+
+end
