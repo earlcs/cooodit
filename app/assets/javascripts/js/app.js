15202644676b21dfdb918f61eb4e8932a2497977
earlcs <earlcasiano@gmail.com>
2020-01-09 15:11:59 +0100

    [UPD] File updated

--- app/assets/javascripts/js/app.js
+++ app/assets/javascripts/js/app.js
@@ -363,24 +363,28 @@
 /* Formulaire de contact */
 
 function openContact(){
-  document.getElementById("contact_formulaire").style.top="80px";
+  $("#contact_formulaire").css("top", "80px");
+  /*document.getElementById("contact_formulaire").style.top="80px";*/
   /*$('#contact_formulaire').focus();*/
 }
  
 function closeContact(){
-  document.getElementById("contact_formulaire").style.top="-950px";
-  history.go(-1);
+  $("#contact_formulaire").css("top", "-950px");
+  /*document.getElementById("contact_formulaire").style.top="-950px";
+  history.go(-1);*/
 }
 
  /* Formulaire Programme */
 
  function openProgram(){
-  document.getElementById("program_formulaire").style.top="80px";
+  $("#program_formulaire").css("top", "80px");
+  /*document.getElementById("program_formulaire").style.top="80px";*/
 }
  
 function closeProgram(){
-  document.getElementById("program_formulaire").style.top="-950px";
-  history.go(-1);
+  $("#program_formulaire").css("top", "-950px");
+  /*document.getElementById("program_formulaire").style.top="-950px";
+  history.go(-1);*/
 }
 
 /* Bouton 'scroll-down' */
