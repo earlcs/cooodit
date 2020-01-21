f87d31131027c1984dec78c1d1d65078566c03e9
earlcs <earlcasiano@gmail.com>
2020-01-21 09:51:40 +0100

    [UPD] File updated

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   
  
 
+  devise_for :users
   get 'pages/job'
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
